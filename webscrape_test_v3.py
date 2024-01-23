25977334a4ff2f60a28607efa2a816d69171adaf
NathanielN55 <123680682+NathanielN55@users.noreply.github.com>
2024-01-23 11:02:31 -0500

    Update webscrape_test_v3.py

--- webscrape_test_v3.py
+++ webscrape_test_v3.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
 import requests
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 import pandas as pd
 
 
@@ -13,9 +13,11 @@ def get_user_videos(driver, url):
     # Open the specified url
     driver.get(url)
     # Allow enough time to move past any sign-in page
-    time.sleep(10)
+    time.sleep(15)
 
     # Scroll through page
+    # Temporarily commented out to limit number of videos to run through
+    ''' 
     scroll_pause_time = 0.5
     screen_height = driver.execute_script("return window.screen.height;")
     i = 1
@@ -29,6 +31,7 @@ def get_user_videos(driver, url):
             time.sleep(20)
             if (screen_height) * i > scroll_height:
                 break 
+    '''
     
     page = BeautifulSoup(driver.page_source, "html.parser")
 
@@ -60,24 +63,25 @@ def audio_to_text():
 
 
 # Function to scrape and store video page data
-def scrape_video_data(driver, url, df):
-    # Open the video url
-    driver.get(url)
+def scrape_video_data(url, df):
+
+    page_request = requests.get(url)
 
     # Open video page as html
-    video_page = BeautifulSoup(driver.page_source, "html.parser")
+    video_page = BeautifulSoup(page_request.content, "html.parser")
+    # print(video_page.prettify())
 
     # Access data to put into the dataframe
     link = url
-    username = video_page.find('span', class_='css-1xccqfx-SpanNickName')
+    username = video_page.find('span', {'data-e2e':'browse-username'})
     user_id = video_page.find('span', class_='css-1c7urt-SpanUniqueId')
     caption = 0
     video_text = audio_to_text()
+    date = 0
 
-    new_row = {"link":link, "username":username, "user_id":user_id, "caption":caption, "video_text":video_text}
-    df = df._append(new_row, ignore_index = True)
-
-
+    new_row = {"username":username, "user_id":user_id, "caption":caption, "video_text":video_text, "date":date, "link":link}
+    #print(new_row)
+    df.loc[len(df)] = new_row
 
 
 
@@ -87,7 +91,7 @@ def scrape_video_data(driver, url, df):
 driver = webdriver.Chrome()
 
 # Create a pandas dataframe to store data
-data_columns = ["link", "username", "user_id", "caption", "video_text"]
+data_columns = ["username", "user_id", "caption", "video_text", "date", "link"]
 df = pd.DataFrame(columns=data_columns)
 
 # Get a list of the videos from the user whose videos we are scraping
@@ -98,6 +102,6 @@ videos = get_user_videos(driver, "https://www.tiktok.com/@dr.kojosarfo")
 
 # For each video in the list of links, get video data
 for video in videos:
-    scrape_video_data(driver,video.a["href"],df)
+    scrape_video_data(video.a["href"],df)
 
-df.display()
+print(df)
